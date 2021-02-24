f6bc58050051fd0afb65e2efad6f8f6dfbd9e993
Jimmy Saenz <jsaenzr@outlook.com>
2021-02-23 19:49:52 -0600

    Remover target=blank en enlaces de asistencia presencial

--- src/map-ext-links.js
+++ src/map-ext-links.js
@@ -9,7 +9,46 @@
 		anchorsWithoutHash.forEach((anchor) => {
 			anchor.setAttribute("target", "_blank");
 		});
-	} 
+	}
+
+	function removeTargetBlankToSpecificLinks() {
+		var linksData = [
+			{
+				pageKey: "id=8866",
+				containerSelector: "#region-bs-main-and-pre",
+				linkKeysByIdParam: ["187", "220", "231", "230", "229", "228"],
+			},
+		];
+
+		function buildSelector(containerSelector, linkKey) {
+			//#container a[href*='id=187']
+			return containerSelector + " a[href*='id=" + linkKey + "']";
+		}
+
+		function processLinkData(linkData) {
+			if (!window.location.search || !window.location.search.includes(linkData.pageKey)) {
+				console.debug("page key not found");
+				return;
+			}
+
+			var mergedSelectors = linkData.linkKeysByIdParam
+				.map((linkKey) => buildSelector(linkData.containerSelector, linkKey))
+				.join(", ");
+
+			if (!mergedSelectors) {
+				console.debug("null merged selectors");
+				return;
+			}
+			var linksToTreat = Array.from(document.querySelectorAll(mergedSelectors));
+			linksToTreat
+				.filter((link) => link.hasAttribute("target"))
+				.forEach((link) => {
+					link.removeAttribute("target");
+				});
+		}
+
+		linksData.forEach((linkData) => processLinkData(linkData));
+	}
 
 	function hideUserNotifications() {
 		var userWrapper = document.getElementById("loggedin-user");
@@ -25,6 +64,7 @@
 			return;
 		}
 		processExternalLinks();
+		removeTargetBlankToSpecificLinks();
 		hideUserNotifications();
 	}
 
